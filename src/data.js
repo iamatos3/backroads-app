7efab0657da38bc4f50df116869180b55918ca76
iamatos3 <matosian93@gmail.com>
2023-05-05 23:42:46 -0400

    Dynamically updated the Tours comp via data.js

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
+import tour5 from './images/tour-5.jpeg';
+import tour6 from './images/tour-6.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'Home' },
   { id: 2, href: '#about', text: 'About' },
@@ -30,5 +37,68 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
+  },
+];
+
+export const tours = [
+  { 
+    id: 1,
+    image: tour1,
+    date: 'August 26th, 2023',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'China',
+    duration: 6,
+    cost: 2100,
+  },
+  { 
+    id: 2,
+    image: tour2,
+    date: 'October 1th, 2022',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Indonesia',
+    duration: 11,
+    cost: 1400,
+  },
+  { 
+    id: 3,
+    image: tour3,
+    date: 'September 15th, 2022',
+    title: 'Explore Hong Kong',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Hong Kong',
+    duration: 8,
+    cost: 5000,
+  },
+  { 
+    id: 4,
+    image: tour4,
+    date: 'December 5th, 2022',
+    title: 'Kenya Highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Kenya',
+    duration: 20,
+    cost: 3300,
+  },
+  { 
+    id: 5,
+    image: tour5,
+    date: 'July 7th, 2022',
+    title: 'South African Extravaganza',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'South Africa',
+    duration: 14,
+    cost: 4500,
+  },
+  { 
+    id: 6,
+    image: tour6,
+    date: 'January 10th, 2021',
+    title: 'Taj Mahal',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'India',
+    duration: 7,
+    cost: 3800,
   },
 ];
